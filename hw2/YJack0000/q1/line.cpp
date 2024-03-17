ec667763107b7d8e72f53d9208c2f3bb4f039c8c
YJack0000 <jackleagends@gmail.com>
2024-03-18 02:21:44 +0800

    fix: size_t -> std::size_t

--- hw2/YJack0000/q1/line.cpp
+++ hw2/YJack0000/q1/line.cpp
@@ -4,7 +4,7 @@
 
 Line::Line() {}
 
-Line::Line(size_t size) : _x(size), _y(size) {}
+Line::Line(std::size_t size) : _x(size), _y(size) {}
 
 Line::Line(Line const &other) : _x(other._x), _y(other._y) {}
 
@@ -24,12 +24,12 @@ Line &Line::operator=(Line &&other) {
 
 Line::~Line() {}
 
-size_t Line::size() const { return _x.size(); }
+std::size_t Line::size() const { return _x.size(); }
 
-float const &Line::x(size_t it) const { return _x[it]; }
+float const &Line::x(std::size_t it) const { return _x[it]; }
 
-float &Line::x(size_t it) { return _x[it]; }
+float &Line::x(std::size_t it) { return _x[it]; }
 
-float const &Line::y(size_t it) const { return _y[it]; }
+float const &Line::y(std::size_t it) const { return _y[it]; }
 
-float &Line::y(size_t it) { return _y[it]; }
+float &Line::y(std::size_t it) { return _y[it]; }
--- hw2/YJack0000/q1/line.hpp
+++ hw2/YJack0000/q1/line.hpp
@@ -1,3 +1,5 @@
+#ifndef __LINE_HPP__
+#define __LINE_HPP__
 #include <vector>
 
 class Line {
@@ -7,15 +9,17 @@ public:
   Line(Line &&);
   Line &operator=(Line const &);
   Line &operator=(Line &&);
-  Line(size_t size);
+  Line(std::size_t size);
   ~Line();
-  size_t size() const;
-  float const &x(size_t it) const;
-  float &x(size_t it);
-  float const &y(size_t it) const;
-  float &y(size_t it);
+  std::size_t size() const;
+  float const &x(std::size_t it) const;
+  float &x(std::size_t it);
+  float const &y(std::size_t it) const;
+  float &y(std::size_t it);
 
 private:
   std::vector<float> _x;
   std::vector<float> _y;
 };
+
+#endif
